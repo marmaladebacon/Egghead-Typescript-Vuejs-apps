2d38c6dba74474df6e00a1c13880d1ece7b96a6e
Matthew Ko <mko@factset.com>
2018-10-19 12:52:04 +0100

    feat(notes): added notes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <h1>{{ message }}</h1>
     <h3>{{ fullMessage }}</h3>
+    <!-- child click -->
     <button @click="clicked">Click</button>
+    <!-- parent click -->
     <button @click="parentClicked">Parent click</button>
   </div>
 </template>
@@ -13,8 +15,12 @@ import Component from 'vue-class-component'
 
 @Component({})
 export default class Hello extends Parent {
+  //class properties are merged in the same fashion as methods
+  //see explanation below
   message: string = 'Hello Vue'
 
+  //Note: Lifecycle hooks are treated differently from methods
+  //They are called in the order of parent to child
   created() {
     console.log('child created')
   }
@@ -22,7 +28,13 @@ export default class Hello extends Parent {
   get fullMessage() {
     return `${this.message} TypeScript`
   }
-
+  //Note: vue-class-component uses the default vue merging strategy
+  //parent clicked method is not triggered even though the parent has
+  // the same named clicked method
+  //WE CANNOT USE THE SUPER KEYWORD
+  //BECAUSE: the vue-class-component is transforming these classes into objects
+  //and the methods are merged from child to parent, so the parent methods are
+  //overwritten by the child's
   clicked() {
     console.log('clicked')
   }
--- src/components/Parent.ts
+++ src/components/Parent.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+//We can use a plain ts file containing a class to reuse logic
 @Component({})
 export default class Parent extends Vue {
   message: string = "Hey from parent"
