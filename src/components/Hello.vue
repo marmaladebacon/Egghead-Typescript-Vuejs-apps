2557f74f36936d9383a340f559192ede7f2a62bc
Matthew Ko <mko@factset.com>
2018-10-19 11:41:49 +0100

    feat(comments): updated comments for learning

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="hello">
     <h1>{{ fullMessage }}</h1>
+    <!-- @ is shorthand for v-on: so  @click === v-on:click -->
     <button @click="clicked">Click</button>
+    <!-- : is shorthand for v-bind: -->
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
+//@ here is used to denote a decorator in typescript land
 @Component({})
 export default class Hello extends Vue {
+  //Data properties in ts-vue are just properties in a class
   message: string = 'Hello Vue'
 
+  //Computed properties in ts-vue are just getters
+  //the below code is equivalent to
+  //computed:{
+  //  fullMessage(){
+  //    return  `${this.message} from TypeScript`;
+  //  }
+  //}
   get fullMessage() {
     return `${this.message} from TypeScript`
   }
-
+  //Lifecycle hooks are written as methods
+  // beforeCreate, created, beforeMount, mounted, beforeUpdate, updated, beforeDestroy, destroyed
   created() {
     console.log('created!')
   }
 
+  //methods are also written as methods
   clicked() {
     console.log('clicked')
   }
