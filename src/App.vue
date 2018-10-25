01f7351c6b78c7329399d138b961cfcc30c82c5c
Matthew Ko <mko@factset.com>
2018-10-25 15:19:17 +0100

    added notes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <button @click="sumUpSimple"> Simple Count {{simpleCount}}</button>
     <button @click="sumUp">Count {{count.acum}}</button>
+
   </div>
 </template>
 
@@ -14,13 +16,27 @@ export default class App extends Vue {
     acum: 0
   }
 
-  @Watch('count', { deep: true })
+  simpleCount: number = 0;
+  //With objects we need deep watching to know when the property has changed
+  @Watch('count', {deep:true})
   watchCount(newVal, oldVal) {
     console.log('newVal: ', newVal, ', oldVal: ', oldVal)
   }
 
+  //With simple properties we don't need {deep:true}
+  @Watch('simpleCount')
+  watchSimpleCount(newVal, oldVal) {
+    console.log('newVal: ', newVal, ', oldVal: ', oldVal);
+  }
+
+  // if we don't use deep:true , we need to assign the property itself for objects eg
+  // this.count = {acum: this.count.acum+=2,};
   sumUp() {
-    this.count.acum++
+    this.count.acum += 2;
+  }
+
+  sumUpSimple(){
+    this.simpleCount+=1;
   }
 }
 </script>
